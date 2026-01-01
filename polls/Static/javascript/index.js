531f9a21c7e718d8120655e8bb9b06c9bbe5ca72
harshad00 <bholo0090@gmail.com>
2026-01-01 22:51:32 +0530

    1JAN2026 in Django

---
+++ polls/Static/javascript/index.js
@@ -0,0 +1,1 @@
+console.log("Hello from polls Static JavaScript index.js!");
---
+++ polls/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Hello harshad in this page.")
+    # return render(render, 'index.html')
+
+def about(request):
+    return HttpResponse("This is About page")
+    # return render(render, 'pages/about.html')
+
+def contact(request):
+    # return HttpResponse("This is my Contact page")
+    return render(render, 'pages/contact.html')
+
+
