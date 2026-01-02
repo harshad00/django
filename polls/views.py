752d7b7e74b514cedefc72646e8037f71c10a4d9
harshad00 <bholo0090@gmail.com>
2026-01-02 22:49:29 +0530

    lear jinja in pyrhon

--- polls/views.py
+++ polls/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello harshad in this page.")
-    # return render(render, 'index.html')
+    # return HttpResponse("Hello harshad in this page.")
+    return render(render, 'index.html')
 
 def about(request):
-    return HttpResponse("This is About page")
-    # return render(render, 'pages/about.html')
+    # return HttpResponse("This is About page")
+    return render(render, 'pages/about.html')
 
 def contact(request):
     # return HttpResponse("This is my Contact page")
